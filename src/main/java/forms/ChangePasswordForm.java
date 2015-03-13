d690b49ded30902bc2b609e6b63fd7f5361dcf4d
ccoves <escorponox@gmail.com>
2015-03-13 14:54:11 +0100

    Typo in message

--- src/main/java/forms/ChangePasswordForm.java
+++ src/main/java/forms/ChangePasswordForm.java
@@ -11,11 +11,11 @@ public class ChangePasswordForm {
     private String username;
 
     @NotNull(message = "Password is empty")
-    @Size(min = 3, message = "Password must be at least 3 charachter long")
+    @Size(min = 3, message = "Password must be at least 3 character long")
     private String newPassword;
 
     @NotNull(message = "Please repeat password")
-    @Size(min = 3, message = "Password must be at least 3 charachter long")
+    @Size(min = 3, message = "Password must be at least 3 character long")
     private String repeatPassword;
 
     public Long getUserId() {
