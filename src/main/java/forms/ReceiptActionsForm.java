0a6efdc349b86faa16a8062375e5cc74d760e0a9
ccoves <escorponox@gmail.com>
2015-03-16 17:31:15 +0100

    ReceiptActionsForm fields

--- src/main/java/forms/ReceiptActionsForm.java
+++ src/main/java/forms/ReceiptActionsForm.java
@@ -3,4 +3,48 @@ package forms;
 public class ReceiptActionsForm {
 
     private Long receivingOrderLine;
+    private String itemCode;
+    private String itemName;
+    private Long pendingQuantity;
+    private Long recQuantity;
+
+    public Long getReceivingOrderLine() {
+        return receivingOrderLine;
+    }
+
+    public void setReceivingOrderLine(Long receivingOrderLine) {
+        this.receivingOrderLine = receivingOrderLine;
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Long getPendingQuantity() {
+        return pendingQuantity;
+    }
+
+    public void setPendingQuantity(Long pendingQuantity) {
+        this.pendingQuantity = pendingQuantity;
+    }
+
+    public Long getRecQuantity() {
+        return recQuantity;
+    }
+
+    public void setRecQuantity(Long recQuantity) {
+        this.recQuantity = recQuantity;
+    }
 }
